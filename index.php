836e29c5ed8f92ff8082bf1c499f2d566f998f44
Vivek Behera <vivekkumarbehera@gmail.com>
2016-04-18 16:52:17 -0800

    changes in index

--- index.php
+++ index.php
@@ -1,8 +1,119 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Smart home</title>
+ 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="media/css" href="style.css">
+</head>
+<body style="overflow: hidden;" >
 
-for($i=0;$i<5;$i++)
-{
- echo "<h1>$i</h1>";
-}
+<nav class="navbar navbar-default navbar-fixed-top role="navigation" style="height:6em" color="white"">
+  <div class="top nav">
+    <div class="navbar-header">
+                      <a class="navbar-brand topnav" href="#home" style="padding-top:0"><img src="images/home.png" alt="Logo" height ="96px"width="180px"></a>
+            </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-?>
+                <ul class="nav navbar-nav navbar-right">
+                 
+      <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#features">Feature</a>
+                    </li>
+                    <li>
+                        <a href="#aboutus">About Us</a>
+                    </li>
+                    <li>
+                        <a href="faqs.php">FAQs</a>
+                    </li>
+                    <li class="active">
+                        <a href="login.php">Connect to my Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <a name="home">
+<div class="container">
+  <div class="banner-info" style="margin-top:6em" >
+    <h1>Your home, <br>in your hands.</h1>
+      <p>Take control of all your appliances</p>
+</div>
+</div> </a>
+
+<div class="content_white">
+  <h2>Smart Home is a comprehensive and affordable home automation solution.</h2>
+  <p>Here's how it can help.</sup>
+</div>
+
+<div class="image2" >
+  <h3>Control Your Home Remotely</h3>
+  <p>Use Smart Home website to control <br> your entire home from anywhere<br> in the world </p>
+</div>
+
+<a name="features">
+<div class="row text-center">
+  <div style=" padding-top:100px;" class="col-mg-3 col-xs-3 feature_grid1"> <img src="media/images (1).jpg">
+        <h4 class="m_1">SAVE ENERGY</h4>
+        <p class="m_2">Switch off electrical appliances and save energy</p>
+        </div>
+      
+      <div style=" padding-top:100px;" class="col-md-3 col-xs-3 feature_grid1"> <img src="media/images (2).jpg">
+        <h4 class="m_1">SAVE MONEY</h4>
+        <p class="m_2">Reudce your electrical bills and save money</p>
+         </div>
+         <div style=" padding-top:100px;" class="col-mg-3 col-xs-3 feature_grid1"> <img src="media/images (3).jpg">
+        <h4 class="m_1">GO ECO FRIENDLY</h4>
+        <p class="m_2"> Save energy and go eco friendly </p>
+        </div>
+      <div style=" padding-top:100px;" class="col-md-3 col-xs-3 feature_grid1"> <img src="media/images (4).jpg">
+        <h4 class="m_1">EASE OF ACESS</h4>
+        <p class="m_2">Easy to acess from far away even</p>
+         </div>
+    </div>
+     </a>
+
+<div class="navigatable" id="contact">
+<div class="featured_content">
+<a name="aboutus">
+  <div class="footer">
+    <div class="row text-center" style="display:inline">
+      <div style="padding-top: 70px;" class="col-mg-3 col-xs-3 feature_grid1 footer" style="text-align:left;"> 
+    
+    <br>
+        269-L, Model Town. <br>
+        near Sanjay Karate, <br>
+        Jalandhar, Punjab. <br>
+        Ph -  +91 9878778424
+        </div> 
+        <div style="padding-top: 70px;" class="col-mg-3 col-xs-3 feature_grid1 footer"style="text-align:center;">
+        <br> Designed by:<br/>
+        Anureet Sandhu<br />
+          Navist Kaur <br />
+</div>
+</div>
+</div> </a>
+</div>
+</div>
+      <script language="javascript">
+        function autoResizeDiv()
+        {
+            document.getElementById('main').style.height = window.innerHeight +'px';
+        }
+        window.onresize = autoResizeDiv;
+        autoResizeDiv();
+    </script>
+</body>
+</html>
+
+
+
+
+
